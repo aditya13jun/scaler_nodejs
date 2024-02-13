af01b911c8f55a68e1b206a7231d13281774f1b0
Aditya <aditya13jun@gmail.com>
2024-02-13 21:27:54 +0530

    commit updated

--- 3_Express/app.js
+++ 3_Express/app.js
@@ -7,16 +7,61 @@ const courses = [
     {id:3, name:'Python'}
 ]
 
+app.use(express.json())
+
 app.get('/' , (req, res)=>{
     res.send('Hello from Scaler Topics!!')
-})
+})  //read data = get()
 
 app.get('/about', (req, res)=>{
     res.send('We Create Impact!!')
 })
 
 app.get('/contact', (req, res)=>{
-    res.send('Comtact us here!!')
+    res.send('Contact us here!!')
+})
+
+app.get('/courses', (req,res)=>{
+    res.send(courses)
+})
+
+app.post('/courses', (req,res)=>{
+    const course = {
+        id : courses.length+1,
+        name : req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+//put method - UPDATING THE DATA
+app.put('/courses/:coursename', (req,res)=>{
+    let course = courses.find(course => course.name === req.params.coursename)
+    if(!course) res.status(404).send('The course you are looking for does not exists!!')
+    res.send(course)
+
+    course.name = req.body.name
+    res.send(course)
+})
+
+//delete
+// app.delete('/courses/:coursename', (req,res)=>{
+//     let UpdatedCourses = courses.filter(course => course.name !== req.params.coursename)
+
+//     courses = UpdatedCourses
+//     res.send(courses)
+// })
+
+app.delete('/courses/:id', (req,res) =>{
+    let course = courses.find(course => course.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('The course you are looking for does not exists!!')
+
+    const index = courses.indexOf(course)
+
+    courses.splice(index,1)
+
+    res.send(course)
+
 })
 
 //Route parameters 
